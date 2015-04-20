89c7888b8fe8613e35e401f2bc07ff5f68535244
dlgltlzed <marc.brueni@hispeed.ch>
2015-04-20 17:33:00 +0200

    Changed to class

--- ShiftParser.py
+++ ShiftParser.py
@@ -4,60 +4,62 @@ import sys
 import json
 
 
-def convert(input_text):
+class ShiftParser:
+    line = 0
+    column = 0
+    position = 0
+    save_coords = False
+    save_position = False
     level = -1
     found_tokens = []
-    for line in input_text.splitlines():
-        if line.strip() != "":
-            if level == -1:
-                count = 0
-                for character in line:
-                    if character == " ":
-                        count += 1
-                    else:
-                        break
-                level = count
-                line = line[count:]
-            else:
-                maximum = level + 1
-                consumed = 0
-                for character in line:
-                    if character == " ":
-                        consumed += 1
-                        if consumed == maximum:
+    types = ('left shift', 'no shift', 'right shift', 'content')
+
+    def addToken(self, type, **kwargs):
+        if type in self.types:
+            kwargs['type'] = type
+            self.found_tokens.append(kwargs)
+        else:
+            raise TypeError("Type '%s' not known" % type)
+
+    def convert(self, input_text):
+        for line in input_text.splitlines():
+            if line.strip() != "":
+                if self.level == -1:
+                    count = 0
+                    for character in line:
+                        if character == " ":
+                            count += 1
+                        else:
                             break
-                    else:
-                        break
-                line = line[consumed:]
-                if consumed < level:
-                    token = {
-                        "type": "left shift",
-                        "count": level - consumed
-                    }
-                    found_tokens.append(token)
-                    level = consumed
-                elif consumed == level:
-                    token = {
-                        "type": "no shift"
-                    }
-                    found_tokens.append(token)
-                else:  # consumed == level + 1
-                    token = {
-                        "type": "right shift"
-                    }
-                    found_tokens.append(token)
-                    level += 1
-            token = {
-                "type": "content",
-                "text": line
-            }
-            found_tokens.append(token)
-    return found_tokens
+                    self.level = count
+                    line = line[count:]
+                else:
+                    maximum = self.level + 1
+                    consumed = 0
+                    for character in line:
+                        if character == " ":
+                            consumed += 1
+                            if consumed == maximum:
+                                break
+                        else:
+                            break
+                    line = line[consumed:]
+                    if consumed < self.level:
+                        self.addToken('left shift',
+                                      count=self.level - consumed)
+                        self.level = consumed
+                    elif consumed == self.level:
+                        self.addToken('no shift')
+                    else:  # consumed == self.level + 1
+                        self.addToken('right shift')
+                        self.level += 1
+                self.addToken('content', text=line)
+        return self.found_tokens
 
 
 if __name__ == "__main__":
     with open(sys.argv[1]) as file:
-        tokens = convert(file.read())
+        tokens = ShiftParser().convert(file.read())
         with open(sys.argv[1] + ".tokens.json", mode="w") as output:
             output.write(json.dumps(tokens))
 
--- Test.py
+++ Test.py
@@ -1,14 +1,14 @@
 __author__ = 'dlgltlzed'
 
 import unittest
-import ShiftParser
+from ShiftParser import ShiftParser
 import json
 
 class TestMethods(unittest.TestCase):
 
     def test_main(self):
         with open("testInput.txt") as input, open("testInput.txt.tokens.json") as output:
-            self.assertEquals(ShiftParser.convert(input.read()), json.loads(output.read()))
+            self.assertEquals(ShiftParser().convert(input.read()), json.loads(output.read()))
 
 if __name__ == '__main__':
     unittest.main()
